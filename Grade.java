a3346817d7a9d3ff7ccb6a6475b4745c0687885b
mitchiipan <116023444+mitchiipan@users.noreply.github.com>
2023-02-10 11:01:52 +0100

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,24 @@
+public class Grade {
+
+
+    private int grade;
+    private TestType testType;
+
+
+    public Grade(int grade, TestType testType) { //TODO int grade validieren lassen , dass da wirklich nur 1 bis 6 geht, oder n ENUM auch für die grades
+        this.grade = grade;
+        this.testType = testType;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public TestType getTestType() {
+        return testType;
+    }
+
+
+
+
+}
